fc398dccfae0177d283956c95e4a8522c198c183
suwan <blacksw1102@gmail.com>
2022-06-08 01:20:52 +0900

    로그인, 로그아웃 추가

--- src/main/java/com/blacksw/sbb/SpringConfig.java
+++ src/main/java/com/blacksw/sbb/SpringConfig.java
@@ -2,24 +2,40 @@ package com.blacksw.sbb;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
+import com.blacksw.sbb.user.UserSecurityService;
+
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 @Configuration
 @EnableWebSecurity
 public class SpringConfig extends WebSecurityConfigurerAdapter {
+
+	private final UserSecurityService userSecurityService;
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
 		http.authorizeRequests().antMatchers("/**").permitAll()
 			.and().csrf().ignoringAntMatchers("/h2-console/**")
-			.and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+			.and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN))
+			.and().formLogin().loginPage("/user/login").defaultSuccessUrl("/")
+			.and().logout().logoutRequestMatcher(new AntPathRequestMatcher("/user/logout")).logoutSuccessUrl("/").invalidateHttpSession(true);
 	}
-	
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userSecurityService).passwordEncoder(passwordEncoder());
+	}
+
 	@Bean
 	public PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
--- src/main/java/com/blacksw/sbb/user/UserRepository.java
+++ src/main/java/com/blacksw/sbb/user/UserRepository.java
@@ -1,7 +1,9 @@
 package com.blacksw.sbb.user;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<SiteUser, Long> {
-
+	Optional<SiteUser> findByUsername(String username);
 }
