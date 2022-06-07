94ad49b92dbd90fd8d0ebe24a07ac65b609bc738
suwan <blacksw1102@gmail.com>
2022-06-07 15:29:53 +0900

    질문 등록  폼 추가

--- src/main/java/com/blacksw/sbb/question/QuestionController.java
+++ src/main/java/com/blacksw/sbb/question/QuestionController.java
@@ -4,8 +4,11 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -28,6 +31,17 @@ public class QuestionController {
 		Question question = this.questionService.getQuestion(id);
 		model.addAttribute("question", question);
 		return "question_detail";
+	}
+
+	@GetMapping("/create")
+	public String questionCreate() {
+		return "question_form";
+	}
+
+	@PostMapping("/create")
+	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
+		questionService.create(subject, content);
+		return "redirect:/question/list";
 	}
 
 }
