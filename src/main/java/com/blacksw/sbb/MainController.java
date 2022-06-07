42f55c34a78c1d64a45fb0c1eda5bee54e3e6388
suwan <blacksw1102@gmail.com>
2022-06-07 11:08:00 +0900

    h2 설치

---
+++ src/main/java/com/blacksw/sbb/MainController.java
@@ -0,0 +1,16 @@
+package com.blacksw.sbb;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+
+	@RequestMapping("/sbb")
+	@ResponseBody
+	public String index() {
+		return "안녕하세요 sbb에 오신것을 환영합니다.";
+	}
+
+}
