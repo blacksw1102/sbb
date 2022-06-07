abf98c101b593579235cc8eb3bfc7e1d83bbf09b
suwan <blacksw1102@gmail.com>
2022-06-07 11:54:48 +0900

    findBySubject 추가

--- src/main/java/com/blacksw/sbb/QuestionRepository.java
+++ src/main/java/com/blacksw/sbb/QuestionRepository.java
@@ -3,5 +3,6 @@ package com.blacksw.sbb;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
+	Question findBySubject(String subject);
 
 }
--- src/test/java/com/blacksw/sbb/SbbApplicationTests.java
+++ src/test/java/com/blacksw/sbb/SbbApplicationTests.java
@@ -1,6 +1,6 @@
 package com.blacksw.sbb;
 
-import java.time.LocalDateTime;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,17 +14,8 @@ class SbbApplicationTests {
 
 	@Test
 	void testJpa() {
-		Question q1 = new Question();
-		q1.setSubject("sbb가 무엇인가요");
-		q1.setContent("sbb에 대해서 알고 싶습니다.");
-		q1.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q1);
-
-		Question q2 = new Question();
-		q2.setSubject("스프링부트 모델 질문입니다.");
-		q2.setContent("id는 자동으로 생성되나요?");
-		q2.setCreateDate(LocalDateTime.now());
-		this.questionRepository.save(q2);
+		Question q = this.questionRepository.findBySubject("sbb가 무엇인가요");
+		assertEquals(1, q.getId());
 	}
 
 	@Test
