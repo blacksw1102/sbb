ac56fd873a74aa3906a510c5529919fad50dd052
suwan <blacksw1102@gmail.com>
2022-06-07 11:30:32 +0900

    Question, Answer 엔티티 추가

---
+++ src/main/java/com/blacksw/sbb/Answer.java
@@ -0,0 +1,35 @@
+package com.blacksw.sbb;
+
+import java.time.LocalDateTime;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Answer {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	@CreatedDate
+	private LocalDateTime createDate;
+
+	@ManyToOne
+	private Question question;
+
+}
---
+++ src/main/java/com/blacksw/sbb/Question.java
@@ -0,0 +1,36 @@
+package com.blacksw.sbb;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+public class Question {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+
+	@Column(length = 200)
+	private String subject;
+
+	@Column(columnDefinition = "TEXT")
+	private String content;
+
+	private LocalDateTime createDate;
+
+	@OneToMany(mappedBy = "question", cascade = CascadeType.REMOVE)
+	private List<Answer> answerList;
+}
