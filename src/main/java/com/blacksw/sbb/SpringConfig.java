1ef90d8aa9051de43f315e3bcb602946ae277b1d
suwan <blacksw1102@gmail.com>
2022-06-07 22:46:50 +0900

    회원가입 추가'

--- src/main/java/com/blacksw/sbb/SpringConfig.java
+++ src/main/java/com/blacksw/sbb/SpringConfig.java
@@ -1,9 +1,12 @@
 package com.blacksw.sbb;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.header.writers.frameoptions.XFrameOptionsHeaderWriter;
 
 @Configuration
@@ -15,6 +18,11 @@ public class SpringConfig extends WebSecurityConfigurerAdapter {
 		http.authorizeRequests().antMatchers("/**").permitAll()
 			.and().csrf().ignoringAntMatchers("/h2-console/**")
 			.and().headers().addHeaderWriter(new XFrameOptionsHeaderWriter(XFrameOptionsHeaderWriter.XFrameOptionsMode.SAMEORIGIN));
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
