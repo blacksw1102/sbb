b3d1911150d5ca0f7b997316b604cf92d0f3993f
suwan <blacksw1102@gmail.com>
2022-06-07 17:46:27 +0900

    질문 등록 폼 생성

--- src/main/java/com/blacksw/sbb/question/QuestionController.java
+++ src/main/java/com/blacksw/sbb/question/QuestionController.java
@@ -2,13 +2,15 @@ package com.blacksw.sbb.question;
 
 import java.util.List;
 
+import javax.validation.Valid;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,14 +36,16 @@ public class QuestionController {
 	}
 
 	@GetMapping("/create")
-	public String questionCreate() {
+	public String questionCreate(QuestionForm questionForm) {
 		return "question_form";
 	}
 
 	@PostMapping("/create")
-	public String questionCreate(@RequestParam String subject, @RequestParam String content) {
-		questionService.create(subject, content);
+	public String questionCreate(@Valid QuestionForm questionForm, BindingResult bindingResult) {
+		if (bindingResult.hasErrors()) {
+			return "question_form";
+		}
+		this.questionService.create(questionForm.getSubject(), questionForm.getContent());
 		return "redirect:/question/list";
 	}
-
 }
