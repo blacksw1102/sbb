79decf1bcb3dc6cb1e8df1f0f74bb85ab9c181da
suwan <blacksw1102@gmail.com>
2022-06-07 14:23:37 +0900

    질문 상세 조회 추가

--- src/main/java/com/blacksw/sbb/question/QuestionController.java
+++ src/main/java/com/blacksw/sbb/question/QuestionController.java
@@ -4,21 +4,30 @@ import java.util.List;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.RequiredArgsConstructor;
 
+@RequestMapping("/question")
 @RequiredArgsConstructor
 @Controller
 public class QuestionController {
 
 	private final QuestionService questionService;
 
-	@RequestMapping("/question/list")
+	@RequestMapping("/list")
 	public String list(Model model) {
 		List<Question> questionList = this.questionService.getList();
 		model.addAttribute("questionList", questionList);
 		return "question_list";
+	}
+
+	@RequestMapping(value = "/detail/{id}")
+	public String detail(Model model, @PathVariable("id") Integer id) {
+		Question question = this.questionService.getQuestion(id);
+		model.addAttribute("question", question);
+		return "question_detail";
 	}
 
 }
--- src/main/java/com/blacksw/sbb/question/QuestionService.java
+++ src/main/java/com/blacksw/sbb/question/QuestionService.java
@@ -1,8 +1,11 @@
 package com.blacksw.sbb.question;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.stereotype.Service;
+
+import com.blacksw.sbb.DataNotFoundException;
 
 import lombok.RequiredArgsConstructor;
 
@@ -14,6 +17,15 @@ public class QuestionService {
 
 	public List<Question> getList() {
 		return this.questionRepository.findAll();
+	}
+
+	public Question getQuestion(Integer id) {
+		Optional<Question> question = this.questionRepository.findById(id);
+		if (question.isPresent()) {
+			return question.get();
+		} else {
+			throw new DataNotFoundException("question not found");
+		}
 	}
 
 }
